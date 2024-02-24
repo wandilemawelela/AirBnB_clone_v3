06be4befa89390aa6675098d4cdc00971f201de5
wandilemawelela <wandile1997@gmail.com>
2024-02-24 08:41:17 -0800

    [ADD] New State

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-init module
+Initializes the API views package
 """
 
 from flask import Blueprint
@@ -10,3 +10,4 @@ from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
+from api.v1.views.states import *
