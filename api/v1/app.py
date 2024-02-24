51bcc69a0a7e6e140a2dd9856d54933a06080167
wandilemawelela <wandile1997@gmail.com>
2024-02-24 08:16:36 -0800

    [ADD] handler for 404 errors that returns a JSON-formatted 404 status code response.

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,7 +5,7 @@ This module serves as the entry point for the API application.
 """
 
 from models import storage
-from flask import Flask
+from flask import Flask, jsonify
 from api.v1.views import app_views
 from os import getenv
 
@@ -33,6 +33,14 @@ def teardown(exception):
         None
     """
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """
+    Handle 404 errors by returning a JSON-formatted response.
+    """
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
