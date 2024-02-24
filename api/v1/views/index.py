03c498db96d68987e7f7c8a8d31a0465c92f56b6
Wandile Mawelela <wandile1997@gmail.com>
2024-02-24 10:23:54 +0200

    [FIX] Formatting

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-index module
+This module contains the routes for the status endpoint of the API.
 """
 
 from api.v1.views import app_views
@@ -9,5 +9,12 @@ from flask import jsonify
 
 @app_views.route('/status', methods=['GET'])
 def status():
-    """ """
+    """
+    status()
+
+    Route that returns a JSON response with the status "OK".
+
+    Returns:
+        JSON: A JSON response with the status "OK".
+    """
     return jsonify({"status": "OK"})
